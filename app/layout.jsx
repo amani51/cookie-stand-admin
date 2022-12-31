f18a1742f01de196e7faff72a6396594fb07d513
amani51 <amani.898919@gmail.com>
2023-01-01 00:59:34 +0200

    feat: create admin Form

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
-import './globals.css'
-
+import "./globals.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -8,7 +9,11 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body class="bg-gray-400 font-sans leading-normal tracking-normal">
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
